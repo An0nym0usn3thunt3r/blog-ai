281292cc627edd9ed705f34e411a26e01a5b20b3
An0nym0usn3thunt3r <139343538+An0nym0usn3thunt3r@users.noreply.github.com>
2024-05-17 08:40:40 +0530

    first commit

---
+++ config/database.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'aws-0-us-east-1.pooler.supabase.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres.ztizrwplgqzdxesdgtma'),
+      password: env('DATABASE_PASSWORD', 'oFAv7OsmCSL9L1om'),
+      ssl: env.bool("DaTABASE_SSL", false),
+    },
+    useNullAsDefault: true,
+  },
+});
